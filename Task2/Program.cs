85c4a01f37c149c30d2c737c55a52a43ce6dd97d
zhyk <zhyk@list.ru>
2023-10-12 20:50:55 +0300

    Task № 3

---
+++ Task2/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда 
+// 5 -> Пятница
+Console.Write("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+// if (day == 1)
+// {Console.WriteLine("Понедельник");}
+// else if (day == 2)
+// {Console.WriteLine("Вторник");}
+// else if (day == 3)
+// {Console.WriteLine("Среда");}
+// else if (day == 4)
+// {Console.WriteLine("Четверг");}
+// else if (day == 5)
+// {Console.WriteLine("Пятница");}
+// else if (day == 6)
+// {Console.WriteLine("Суббота");}
+// else if (day == 7)
+// {Console.WriteLine("Воскресенье");}
+// else
+// {Console.WriteLine("Это не день недели");}
+
+switch(day)
+{
+    case 1:
+        {Console.WriteLine("Понедельник");}
+        break;
+    case 2:
+        {Console.WriteLine("Вторник");}
+        break;
+    case 3:
+        {Console.WriteLine("Среда");}
+        break;
+    case 4:
+        {Console.WriteLine("Четверг");}
+        break;
+    case 5:
+        {Console.WriteLine("Пятница");}
+        break;
+    case 6:
+        {Console.WriteLine("Суббота");}
+        break;
+    case 7:
+        {Console.WriteLine("Воскресенье");} 
+        break; 
+    default:
+        {Console.WriteLine("Это не день недели");}
+        break;
+
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number <= 999)
+{
+int digitlast = number % 10;
+Console.Write(digitlast);
+}
+else
+{
+ Console.Write("Введите трехзначное число: ");   
+}
