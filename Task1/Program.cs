387484bd6b26d964730fe4fd3b324622f476aa38
zhyk <zhyk@list.ru>
2023-10-12 19:42:39 +0300

    Task № 2

---
+++ Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2)
+{Console.WriteLine("Первое число является квадратом второго");} 
+else
+{Console.WriteLine("Первое число не является квадратом второго");}
+
