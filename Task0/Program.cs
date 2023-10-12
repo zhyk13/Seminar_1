4101424df64164283f3385020dfee769f064a16d
zhyk <zhyk@list.ru>
2023-10-12 19:33:11 +0300

    Task № 1

---
+++ Task0/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine()); // "5"->5
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {square}");
